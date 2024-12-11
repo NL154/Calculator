e97a91dc09c755fc69da4a4d344325412e88d527
NL154 <lnie12089@gmail.com>
2024-12-11 16:11:12 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        btntotal = New Button()
+        div = New Button()
+        btnx = New Button()
+        subtr = New Button()
+        btnp = New Button()
+        btnclear = New Button()
+        Button9 = New Button()
+        Button0 = New Button()
+        TextBox1 = New TextBox()
+        btnsqrt = New Button()
+        Button8 = New Button()
+        Button1 = New Button()
+        Button7 = New Button()
+        Button2 = New Button()
+        Button6 = New Button()
+        Button3 = New Button()
+        Button5 = New Button()
+        Button4 = New Button()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(btntotal)
+        Panel1.Controls.Add(div)
+        Panel1.Controls.Add(btnx)
+        Panel1.Controls.Add(subtr)
+        Panel1.Controls.Add(btnp)
+        Panel1.Controls.Add(btnclear)
+        Panel1.Controls.Add(Button9)
+        Panel1.Controls.Add(Button0)
+        Panel1.Controls.Add(TextBox1)
+        Panel1.Controls.Add(btnsqrt)
+        Panel1.Controls.Add(Button8)
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(Button7)
+        Panel1.Controls.Add(Button2)
+        Panel1.Controls.Add(Button6)
+        Panel1.Controls.Add(Button3)
+        Panel1.Controls.Add(Button5)
+        Panel1.Controls.Add(Button4)
+        Panel1.Location = New Point(24, 32)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(384, 376)
+        Panel1.TabIndex = 0
+        ' 
+        ' btntotal
+        ' 
+        btntotal.Location = New Point(280, 248)
+        btntotal.Name = "btntotal"
+        btntotal.Size = New Size(75, 23)
+        btntotal.TabIndex = 14
+        btntotal.Text = "="
+        btntotal.UseVisualStyleBackColor = True
+        ' 
+        ' div
+        ' 
+        div.Location = New Point(280, 200)
+        div.Name = "div"
+        div.Size = New Size(75, 23)
+        div.TabIndex = 13
+        div.Text = "/"
+        div.UseVisualStyleBackColor = True
+        ' 
+        ' btnx
+        ' 
+        btnx.Location = New Point(272, 168)
+        btnx.Name = "btnx"
+        btnx.Size = New Size(75, 23)
+        btnx.TabIndex = 12
+        btnx.Text = "x"
+        btnx.UseVisualStyleBackColor = True
+        ' 
+        ' subtr
+        ' 
+        subtr.Location = New Point(272, 120)
+        subtr.Name = "subtr"
+        subtr.Size = New Size(75, 23)
+        subtr.TabIndex = 11
+        subtr.Text = "-"
+        subtr.UseVisualStyleBackColor = True
+        ' 
+        ' btnp
+        ' 
+        btnp.Location = New Point(272, 72)
+        btnp.Name = "btnp"
+        btnp.Size = New Size(75, 23)
+        btnp.TabIndex = 10
+        btnp.Text = "+"
+        btnp.UseVisualStyleBackColor = True
+        ' 
+        ' btnclear
+        ' 
+        btnclear.Location = New Point(184, 200)
+        btnclear.Name = "btnclear"
+        btnclear.Size = New Size(75, 23)
+        btnclear.TabIndex = 3
+        btnclear.Text = "Clear"
+        btnclear.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(184, 168)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(75, 23)
+        Button9.TabIndex = 9
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button0
+        ' 
+        Button0.Location = New Point(96, 200)
+        Button0.Name = "Button0"
+        Button0.Size = New Size(75, 23)
+        Button0.TabIndex = 2
+        Button0.Text = "0"
+        Button0.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(8, 8)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(216, 48)
+        TextBox1.TabIndex = 1
+        ' 
+        ' btnsqrt
+        ' 
+        btnsqrt.Location = New Point(8, 200)
+        btnsqrt.Name = "btnsqrt"
+        btnsqrt.Size = New Size(75, 23)
+        btnsqrt.TabIndex = 1
+        btnsqrt.Text = "Button10"
+        btnsqrt.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(96, 168)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(75, 23)
+        Button8.TabIndex = 8
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(8, 72)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(8, 168)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(75, 23)
+        Button7.TabIndex = 7
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(96, 72)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 2
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(184, 128)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(75, 23)
+        Button6.TabIndex = 6
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(184, 72)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 3
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(96, 128)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 23)
+        Button5.TabIndex = 5
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(8, 128)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 4
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Panel1)
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button9 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents btntotal As Button
+    Friend WithEvents div As Button
+    Friend WithEvents btnx As Button
+    Friend WithEvents subtr As Button
+    Friend WithEvents btnp As Button
+    Friend WithEvents btnclear As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents btnsqrt As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,91 @@
+﻿Imports System.Net.Security
+Imports System.Math
+
+
+Public Class Form1
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button8.Click, Button9.Click, Button0.Click, btnx.Click, div.Click
+        Dim value = 0
+        Dim btn = CType(sender, Button)
+        TextBox1.Text = TextBox1.Text + btn.Text
+    End Sub
+    Dim Operation As String
+    Dim Currentvalue As String
+
+
+
+    Private Sub total(sender As Object, e As EventArgs) Handles btntotal.Click
+        Try
+            Select Case Operation
+                Case "-"
+                    Dim re As String = TextBox1.Text
+                    Dim result As String
+                    result = (Currentvalue) - (re)
+                    TextBox1.Text = result
+                Case "+"
+                    Dim news As String = TextBox1.Text
+                    Dim result As String
+                    result = Val(news) + Val(Currentvalue)
+                    TextBox1.Text = result
+                Case "x"
+                    Dim rt As String = TextBox1.Text
+                    Dim result As String
+                    result = Currentvalue * rt
+                    TextBox1.Text = result
+                Case "/"
+                    Dim di As String = TextBox1.Text
+                    Dim result As String
+                    result = Currentvalue / di
+                    TextBox1.Text = result
+            End Select
+        Catch ex As Exception
+            MessageBox.Show("Invalid input.")
+            TextBox1.Clear()
+        End Try
+
+    End Sub
+    Private Sub btnsqrt_Click(sender As Object, e As EventArgs) Handles btnsqrt.Click
+        Try
+            Dim upt As String = TextBox1.Text
+            Dim result As Double = Math.Sqrt(upt)
+            TextBox1.Text = result
+        Catch ex As Exception
+            MessageBox.Show("Invalid")
+            TextBox1.Clear()
+        End Try
+    End Sub
+    Private Sub clear_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+        TextBox1.Clear()
+    End Sub
+    Private Sub p(sender As Object, e As EventArgs) Handles btnp.Click
+        Operation = "+"
+        Currentvalue = TextBox1.Text
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub subtr_Click(sender As Object, e As EventArgs) Handles subtr.Click
+        Operation = "-"
+        Currentvalue = TextBox1.Text
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub btnx_Click(sender As Object, e As EventArgs) Handles btnx.Click
+        Operation = "x"
+        Currentvalue = TextBox1.Text
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub div_Click(sender As Object, e As EventArgs) Handles div.Click
+        Operation = "/"
+        Currentvalue = TextBox1.Text
+        TextBox1.Clear()
+    End Sub
+End Class
